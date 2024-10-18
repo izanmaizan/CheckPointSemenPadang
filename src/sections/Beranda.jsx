c8a5e124d7b2dc1f41ba720a53c128b8aec1e1da
izanmaizan <111477612+izanmaizan@users.noreply.github.com>
2024-10-18 11:17:35 +0700

    fix

--- src/sections/Beranda.jsx
+++ src/sections/Beranda.jsx
@@ -162,7 +162,7 @@ const Beranda = () => {
                 </>
               ) : role === "petugas" ? (
                 <>
-                <div className="col-span-2">
+                <div className="col-span-3 gap-4 max-w-lg grid grid-cols-2">
                   <Link to="/check-point">
                     <button className="text-white bg-[#0e7490] hover:bg-[#0c647a] focus:ring-4 focus:outline-none focus:ring-[#0c647a] font-medium rounded-lg text-sm w-full px-5 py-4 transition-all">
                       Check Point
